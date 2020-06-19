05df9e10c7363c18c4f60ac465dd010d2dcb4a20
MaryJay787 <josephmaryam.mj@gmail.com>
2020-06-19 16:41:29 +0000

    created game and end game components and installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import GamePage from './components/GamePage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-         A Trivia App
-        </header>
-        <button id="str-game-btn">Start Game</button>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+          A Trivia App
+          </header>
+          <Link to={'/gamepage'} className='link-to-game-page'><button id="str-game-btn">Start Game</button></Link>
+          <Switch>
+            <Route path='/gamepage' component={GamePage}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/EndGamePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EndGamePage extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Thank You for Playing!</h1>
+            </div>
+        );
+    }
+}
+
+export default EndGamePage;
