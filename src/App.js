9f6739481ec279ba3535b87d8a5c9db7ec924dfd
MaryJay787 <josephmaryam.mj@gmail.com>
2020-06-26 00:37:07 +0000

    wiring questions to game page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import React, {Component} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import GamePage from './components/GamePage';
-import { getCategories } from './fetches/backend';
+import { getCategories, getPOIQuestions } from './fetches/backend';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {categories: [], 
-                  renderGamePage: false};
+    this.state = {categories: [], questions: []};
   }
   componentDidMount(){
     getCategories().then(all_categories => this.setState({categories: all_categories.map(item => (item.title))}));
     // getCategories().then(all_categories => console.log(all_categories))
+    getPOIQuestions().then(questions => this.setState({questions: questions}));
 
   };
-
-  // handleClick(e){
-  //   this.setState({renderGamePage: true});
-  // };
-
+ 
   render() {
     return (
       <Router>
@@ -31,14 +27,12 @@ class App extends Component {
           <h1>Welcome to A Trivia Game</h1>
           <p>Click "Start Game" to Begin!!!</p>
           <Link to={'/gamepage'} className='link-to-game-page'>
-            <button type="button" onClick={this.handleClick} className="str-game-btn">Start Game</button>
+            <button type="button" className="str-game-btn">Start Game</button>
           </Link>
           </div>
           <Switch>
-            <Route path='/gamepage'  render={() => (<GamePage categories={this.state.categories}/>)} />
+            <Route path='/gamepage'  render={() => (<GamePage categories={this.state.categories} questions={this.state.questions}/>)} />
           </Switch>
-
-          {/* {this.state.renderGamePage ? <GamePage /> : 'No Game Page'} */}
         </div>
       </Router>
     );
